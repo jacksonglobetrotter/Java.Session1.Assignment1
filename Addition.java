ea89a277e15babfcddad5ab395f14a1267de2453
jacksonkailath <jacksonkailath@gmail.com>
2016-10-03 17:50:13 +0530

    assignment 1

---
+++ Addition.java
@@ -0,0 +1,18 @@
+
+public class Addition {
+public static void main(String[] args){
+	int i;
+	float a;
+	i=10;
+	a=(float)1.1;
+	int j;
+	j=(int)(i+a);
+	System.out.println(j);
+	
+	
+	
+	
+	
+	
+}
+}
